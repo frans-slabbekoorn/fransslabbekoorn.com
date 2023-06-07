13b5ceba19d3d0a3dc0770c46657f5c7494ab690
JoanVerhulst <verhulstjoan@gmail.com>
2023-06-07 20:33:31 +0200

    feat: Add metadata meta and openGraph tags for seo optimalization.
    This adds the following meta tags, title, description, favicon, keywords, color scheme and theme color, opengraph title, description and image.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import React, { type ReactNode } from 'react';
 
+import { type Metadata } from 'next';
+
 import '@material-design-icons/font/filled.css';
 import General_Sans from 'next/font/local';
 
@@ -19,6 +21,31 @@ const RootLayout = ({ children }: Props) => {
             <body className="overflow-x-hidden">{children}</body>
         </html>
     );
+};
+
+export const metadata: Metadata = {
+    title: 'Frans Slabbekoorn | Software Developer',
+    description:
+        "I'm a software developer based in Ouddorp, Netherlands, with expertise in backend development. Skilled in Laravel, Javascript, API's, and databases, I craft efficient solutions for complex problems.",
+    icons: { icon: '/assets/icons/favicon.png' },
+    keywords: ['frans slabbekoorn', 'ouddorp', 'software developer', 'backend developer'],
+    colorScheme: 'light',
+    themeColor: '#E8F0F5',
+    openGraph: {
+        title: 'Frans Slabbekoorn',
+        description:
+            "I'm a software developer based in Ouddorp, Netherlands, with expertise in backend development. Skilled in Laravel, Javascript, API's, and databases, I craft efficient solutions for complex problems.",
+        url: 'https://fransslabbekoorn.com',
+        siteName: 'fransslabbekoorn.com',
+        type: 'website',
+        images: [
+            {
+                url: '/assets/images/ogimage.png',
+                width: 1200,
+                height: 630,
+            },
+        ],
+    },
 };
 
 export default RootLayout;
